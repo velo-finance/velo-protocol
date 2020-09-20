6420c55f7f6f8beeb6d0319adfad2c310d413106
ziggyetherdust <71156973+ziggyetherdust@users.noreply.github.com>
2020-09-20 12:40:16 +0200

    Execute arbitrary calls on rebase

--- contracts/rebaser/VELORebaser.sol
+++ contracts/rebaser/VELORebaser.sol
@@ -15,13 +15,45 @@ contract VELORebaser {
 
     address public VELO;
 
+    /// @notice Governance address
+    address public gov;
+
+    // Stable ordering is not guaranteed.
+    Transaction[] public transactions;
+
     // TODO rebase events
 
+    struct Transaction {
+        address destination;
+        bytes data;
+    }
+
+    modifier onlyGov() {
+        require(msg.sender == gov, "!gov");
+        _;
+    }
 
     constructor(
         address _VELO
     ) public {
         VELO = _VELO;
+        gov = msg.sender;
+    }
+
+    function setGov(address newGov) external onlyGov {
+        gov = newGov;
+    }
+
+    function addTransaction(address destination, bytes memory data) external onlyGov {
+        transactions.push(Transaction({
+            destination: destination,
+            data: data
+        }));
+    }
+
+    function removeTransaction(uint256 index) external onlyGov {
+        transactions[index] = transactions[transactions.length];
+        transactions.pop();
     }
 
     function rebase() external {
@@ -43,9 +75,15 @@ contract VELORebaser {
 
         lastRebase = block.timestamp;
 
-        // TODO sync Uniswap pool
-        // TODO sync Sushiswap pool
-        // TODO possibly sync balancer pools
+        _afterRebase();
+    }
+
+    function _afterRebase() internal {
+        for(uint256 i = 0; i < transactions.length; i ++) {
+            Transaction memory transaction = transactions[i];            
+            // Failed transactions should be ignored
+            transaction.destination.call(transaction.data);
+        }
     }
 
 }
