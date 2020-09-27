034af2eabb753bbcc11e84b44791cb298446369c
ziggyetherdust <71156973+ziggyetherdust@users.noreply.github.com>
2020-09-27 13:31:45 +0200

    remove ui from this repo

--- yam-www/src/constants/tokenAddresses.ts
+++
@@ -1,2 +0,0 @@
-export const velo = '0x0e2298e3b3390e3b945a5456fbf59ecc3f55da16'
-export const velov2 = '0xaba8cac6866b83ae4eec97dd07ed254282f6ad8a'
--- yam-www/src/hooks/useYam.ts
+++
@@ -1,9 +0,0 @@
-import { useContext } from 'react'
-import { Context } from '../contexts/YamProvider'
-
-const useYam = () => {
-  const { velo } = useContext(Context)
-  return velo
-}
-
-export default useYam
